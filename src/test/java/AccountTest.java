0062f2fa21c836d676cc7724c888804eaaf268a2
Darryl Moynihan <darryl.moynihan@students.ittralee.ie>
2019-04-30 21:34:14 +0100

    Jenkins build test2

--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -2,6 +2,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class AccountTest {
+
     Account a = new Account();
     //test balance value when object created
     @Test
